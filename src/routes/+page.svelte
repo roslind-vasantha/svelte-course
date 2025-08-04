4c2b7cb78d36a2cd7a1a51768b69149f77f30c16
roslind-vasantha <roslindvasantha@gmail.com>
2025-08-04 22:08:49 +0530

    feat: :sparkles: JS proxies and object getters & setters

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,88 +1,55 @@
 <script lang="ts">
-	import Button from '$lib/components/Button.svelte';
-	// import DisplayName from '$lib/components/DisplayName.svelte';
-	// import RandomNumber from '$lib/components/RandomNumber.svelte';
-	// import Counter from '$lib/components/Counter.svelte';
+	const target = {
+		firstName: 'Roslind',
+		lastName: 'Vasantha',
+		occupations: [],
 
-	import { Search, Baby, AlarmClock } from '@lucide/svelte';
+		get fullName() {
+			return `${this.firstName} ${this.lastName}`;
+		},
 
-	let html = '<p>Hello</p>';
-	let button: Button;
+		set occupation(value: string) {
+			console.log(`Adding ${value} to occupations`);
+			this.occupations.push(value);
+		}
+	};
 
-	$effect(() => {
-		// console.log(button.getButton());
-		button.getButton().focus();
-		// button.focus();
-	});
+	// target.firstName = 'Afroze';
+	// console.log(target.fullName);
+
+	target.occupation = 'Web dev';
+	console.log(target);
+
+	const handler = {
+		get(target, prop) {
+			// console.log(target, prop);
+			// return 'Hello';
+			return prop in target ? target[prop] : 'NA';
+		},
+
+		set(target, prop, value) {
+			console.log(`Setting; ${prop} = ${value}`);
+
+			if (['firstName', 'lastName'].includes(prop)) {
+				if (typeof value !== 'string') {
+					throw new TypeError(`Property ${prop} must be a string.`);
+				}
+			}
+
+			target[prop] = value;
+			return true;
+		}
+	};
+
+	const proxy = new Proxy(target, handler);
+
+	proxy.firstName = 'Zubae';
+	// proxy.lastName = 2;
+	console.log(proxy.firstName);
 </script>
-
-<!-- {#snippet left()}
-	<Search />
-{/snippet} -->
-
-<!-- <DisplayName /> -->
-<!-- <RandomNumber /> -->
-<!-- <Counter /> -->
-<div class="wrapper">
-	{@html html}
-	<div
-		role="presentation"
-		onclick={(e) => {
-			console.log('event coming from div');
-		}}
-	>
-		<Button
-			bind:this={button}
-			href={undefined}
-			size="lg"
-			shadow
-			aria-label="up"
-			--buttonBgColor="yellow"
-			--buttonTextColor="green"
-			onclick={(e) => {
-				e.stopPropagation();
-				console.log('event coming from button');
-			}}
-			leftHover={() => {
-				console.log('left hovered');
-			}}
-		>
-			{#snippet left(isHovered: boolean)}
-				{#if isHovered}
-					<Search />
-				{:else}
-					<AlarmClock />
-				{/if}
-			{/snippet}
-
-			<!-- Need to find out why it's not working when  -->
-			<!-- {#snippet children(isHovered: boolean)}
-		Text {isHovered}
-	{/snippet} -->
-
-			Text
-			{#snippet right()}
-				<Baby />
-			{/snippet}
-		</Button>
-	</div>
-</div>
 
 <style>
 	:global(body) {
 		background-color: #222;
-	}
-
-	@keyframes --global-move {
-	}
-
-	.wrapper :global {
-		/* button {
-			background-color: blue !important;
-		} */
-
-		p {
-			color: white;
-		}
 	}
 </style>
