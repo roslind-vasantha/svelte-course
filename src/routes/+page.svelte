b2bcf42855739b161da6080ae111e46dfd7dd0f5
roslind-vasantha <roslindvasantha@gmail.com>
2025-08-05 12:55:59 +0530

    feat: :sparkles: Deep state reactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,61 @@
 <script lang="ts">
-	const target = {
-		firstName: 'Roslind',
-		lastName: 'Vasantha',
-		occupations: [],
-
-		get fullName() {
-			return `${this.firstName} ${this.lastName}`;
-		},
-
-		set occupation(value: string) {
-			console.log(`Adding ${value} to occupations`);
-			this.occupations.push(value);
+	let array = $state([1, 2, 3, 4]);
+	let objectsArray = $state([{ id: 1 }, { id: 2 }]);
+	let object = $state({
+		firstName: 'Ali',
+		lastName: 'Afroze',
+		address: {
+			city: 'City',
+			street: 'Street'
 		}
-	};
+	});
 
-	// target.firstName = 'Afroze';
-	// console.log(target.fullName);
+	$effect(() => {
+		console.log('object effect');
+		console.log(object);
+	});
+	$effect(() => {
+		console.log('object.firstName effect');
+		console.log(object.firstName);
+	});
+	$effect(() => {
+		console.log('object.address.city');
+		console.log(object.address.city);
+	});
+	$effect(() => {
+		console.log('array');
+		// console.log(array);
+		console.log(array[0]);
+	});
+	$effect(() => {
+		console.log('array.length');
+		console.log(array.length);
+	});
 
-	target.occupation = 'Web dev';
-	console.log(target);
-
-	const handler = {
-		get(target, prop) {
-			// console.log(target, prop);
-			// return 'Hello';
-			return prop in target ? target[prop] : 'NA';
-		},
-
-		set(target, prop, value) {
-			console.log(`Setting; ${prop} = ${value}`);
-
-			if (['firstName', 'lastName'].includes(prop)) {
-				if (typeof value !== 'string') {
-					throw new TypeError(`Property ${prop} must be a string.`);
-				}
-			}
-
-			target[prop] = value;
-			return true;
-		}
-	};
-
-	const proxy = new Proxy(target, handler);
-
-	proxy.firstName = 'Zubae';
-	// proxy.lastName = 2;
-	console.log(proxy.firstName);
+	// This leads to an infinite loop
+	// $effect(() => {
+	// 	array.push(1);
+	// });
 </script>
+
+<h1>{object.firstName}</h1>
+<h2>{object.address.city}</h2>
+
+<input bind:value={object.firstName} />
+<input bind:value={object.address.city} />
+<input bind:value={object.address.street} />
+<p>{array}</p>
+<button
+	onclick={() => {
+		// array.push(Math.round(Math.random() * 10));
+		// array[0] = Math.round(Math.random() * 10);
+		array[1] = Math.round(Math.random() * 10);
+	}}>Add to array</button
+>
 
 <style>
 	:global(body) {
 		background-color: #222;
+		color: #fff;
 	}
 </style>
